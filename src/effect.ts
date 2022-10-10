9b4c643d5dd75c4a51177a51d408895dc2805539
osace <381187081@qq.com>
2022-10-10 18:01:00 +0800

    feat: effect options

--- src/effect.ts
+++ src/effect.ts
@@ -1,18 +1,26 @@
 let activeEffect:ReactiveEffect
 
-export function effect(fn:Function){
-  const _effect = new  ReactiveEffect(fn)
+export function effect(fn:Function,options: any = {}){
+  const scheduler = options.scheduler
+  const _effect = new  ReactiveEffect(fn,scheduler)
   _effect.run()
+  const runner: any = _effect.run.bind(_effect)
+  runner.effect = _effect
+  return runner
 }
 
 class ReactiveEffect {
   private _fn: Function
-  constructor(fn:Function){
+  deps = []
+  constructor(fn:Function,public scheduler?){
     this._fn = fn
   }
   run() {
     activeEffect = this
     this._fn()
+  }
+  stop(){
+    cleanupEffect(this)
   }
 }
 
@@ -31,6 +39,10 @@ export function track(target, key){
   trackEffects(dep)
 }
 
+function cleanupEffect(effect){
+  effect
+}
+
 export function trackEffects(dep){
   dep.add(activeEffect)
 }
@@ -38,14 +50,16 @@ export function trackEffects(dep){
 export function trigger(target, key){
   const depsMap = targetMap.get(target)
   let dep = depsMap.get(key)
-  for(const effect of dep){
-    effect.run()
-  }
+  triggerEffects(dep)
 }
 
 export function triggerEffects(dep){
   for(const effect of dep){
-    effect.run()
+    if(effect.scheduler){
+      effect.scheduler()
+    }else{
+      effect.run()
+    }
   }
 }
 
--- test/effect.spec.ts
+++ test/effect.spec.ts
@@ -15,4 +15,52 @@ describe('effect',()=>{
     user.age++
     expect(nextAge).toBe(12)
   })
+
+  it('should return runner when call effect', ()=>{
+    // effect(fn) -> function(runner) -> fn -> return
+    let foo = 10
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+
+    expect(foo).toBe(11)
+    const r = runner()
+    expect(foo).toBe(12)
+    expect(r).toBe("foo")
+  })
+
+  it('description', () => {
+    /**
+     * 1. 通过effect的第二个参数给定的一个scheduler的fn
+     * 2. effect第一次执行的时候 还会执行fn
+     * 3. 当响应式对象 set update不会执行fn 而是执行 scheduler
+     * 4. 如果说当执行runner的时候 会再次执行fn
+     */
+    let dummy;
+    let run: any;
+
+    const scheduler = jest.fn(()=>{
+      run = runner
+    })
+
+    const obj = reactive({foo:1})
+    const runner = effect(()=>{
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
+  })
 })
